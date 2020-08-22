5dc1ca77b297b7035d90c56ce686cdfc88c00b76
Ahmed Ramadan <AhmedShouman53@gmail.com>
2020-08-22 03:43:08 +0200

    start adding the HomeFragment layout and activity_list_item.xml

--- app/src/main/java/com/shouman/apps/reseller/admin/ui/main/customerProfileFragment/CustomerProfileFragment.kt
+++ app/src/main/java/com/shouman/apps/reseller/admin/ui/main/customerProfileFragment/CustomerProfileFragment.kt
@@ -7,6 +7,7 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.fragment.findNavController
 import com.google.android.material.snackbar.Snackbar
 import com.shouman.apps.reseller.admin.R
 import com.shouman.apps.reseller.admin.databinding.CustomerProfileFragmentBinding
@@ -38,6 +39,9 @@ class CustomerProfileFragment : Fragment() {
 
             lifecycleOwner = this@CustomerProfileFragment
 
+            materialToolbar.setNavigationOnClickListener {
+                findNavController().popBackStack()
+            }
         }
 
         return mBinding.root
